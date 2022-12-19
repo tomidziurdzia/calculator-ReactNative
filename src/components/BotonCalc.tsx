abb6422bf9a1f3e7f4097eef16387747f7581375
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-12-20 09:36:35 +1000

    Diseno terminado de calculadora

---
+++ src/components/BotonCalc.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+interface Props {
+  texto: string;
+  color?: string;
+  ancho?: boolean;
+}
+
+const BotonCalc = ({texto, color = '#2D2D2D', ancho = false}: Props) => {
+  return (
+    <TouchableOpacity>
+      <View
+        style={{
+          ...styles.boton,
+          backgroundColor: color,
+          width: ancho ? 180 : 80,
+        }}>
+        <Text
+          style={{
+            ...styles.botonTexto,
+            color: color === '#9B9B9B' ? 'black' : 'white',
+          }}>
+          {texto}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default BotonCalc;
